e682eddd55f64c9fdc7119cce5746f25824fb0eb
Jithinkj23 <jithinkjnsm2000@gmail.com>
2024-09-24 11:13:49 +0530

    Add files via upload

---
+++ spring/src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,29 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.model.Book;
+import com.example.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    // Implement other methods as needed (getBookById, updateBook, deleteBook, etc.)
+}
